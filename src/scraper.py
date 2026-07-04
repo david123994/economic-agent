b4b4482d1d40aef06051624d590a8839dfc00dc4
david123994 <tesco.avi@gmail.com>
2026-07-04 07:47:18 +0300

    Update scraper.py

--- src/scraper.py
+++ src/scraper.py
@@ -11,7 +11,8 @@ RSS_FEEDS = {
     "גלובס": "https://www.globes.co.il/webservice/rss/rssfeeder.asmx/FeederNode?iID=585",
     "גלובס שוק ההון": "https://www.globes.co.il/webservice/rss/rssfeeder.asmx/FeederNode?iID=1111",
     "גלובס נדלן": "https://www.globes.co.il/webservice/rss/rssfeeder.asmx/FeederNode?iID=1170",
-    "ינט כלכלה": "https://www.ynet.co.il/Integration/StoryRss2.xml",
+   "רויטרס עברית": "https://feeds.reuters.com/reuters/ilTopNews",
+"TheMarker": "https://www.themarker.com/srv/rss/themarker-all.xml",
     "מאקו כלכלה": "https://rcs.mako.co.il/rss/31750a2610f26110VgnVCM1000005201000aRCRD.xml",
 }
 
